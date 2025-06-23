25176e1b08293542e680ca8586585493ba5ad52f
Peter Fajdiga <peter.fajdiga@gmail.com>
2025-06-23 22:59:25 +0200

    initial commit

---
+++ src/go.mod
@@ -0,0 +1,5 @@
+module iplan
+
+go 1.24.2
+
+require github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d // indirect
---
+++ src/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/acarl005/stripansi"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		coloredLine := scanner.Text()
+		rawLine := stripansi.Strip(coloredLine)
+		indentation := getIndentation(rawLine)
+		fmt.Println(indentation, coloredLine)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error reading input:", err)
+	}
+}
+
+func getIndentation(line string) int {
+	spaces := countPrefixedSpaces(line)
+	spaces += matchedPrefixLen(line[spaces:])
+	return spaces
+}
+
+func countPrefixedSpaces(str string) int {
+	count := 0
+	for _, char := range str {
+		if char == ' ' {
+			count++
+		} else {
+			break
+		}
+	}
+	return count
+}
+
+func matchedPrefixLen(str string) int {
+	prefixes := []string{"- ", "+ ", "~ ", "<= "}
+	for _, prefix := range prefixes {
+		if strings.HasPrefix(str, prefix) {
+			return len(prefix)
+		}
+	}
+	return 0
+}
